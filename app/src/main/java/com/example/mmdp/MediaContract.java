27b388ff54cdcb2a6831f3a2eb9389ef946241a5
mohameDMEDO97 <mohamedtalaat0111790@gmail.com>
2019-07-21 16:05:54 +0200

    creating database

---
+++ app/src/main/java/com/example/mmdp/MediaContract.java
@@ -0,0 +1,39 @@
+package com.example.mmdp;
+
+import android.provider.BaseColumns;
+
+public final class Contract {
+
+
+    public static final class MediaEntry implements BaseColumns{
+
+
+        public static final String TABLE_NAME = "media";
+        public static final String COLUMN_TITLE = " title";
+        public static final String COLUMN_YEAR = "year";
+        public static final String COLUMN_RATED = "rated";
+        public static final String COLUMN_RUN_TIME = "runtime";
+        public static final String COLUMN_DIRECTOR = "director";
+        public static final String COLUMN_ACTORS = "actors";
+        public static final String COLUMN_WRITERs = "writers";
+        public static final String COLUMN_PLOT = "plot";
+        public static final String COLUMN_LANGUAGE = "language";
+        public static final String COLUMN_COUNTRY = "country";
+        public static final String COLUMN_AWARRDS = "awards";
+        public static final String COLUMN_TYPE = "type";
+        public static final String COLUMN_IMDB_RATING = "imdb";
+        public static final String COLUMN_ROOTEN_RATING = "rotten";
+        public static final String COLUMN_BOXOFFICE = "boxoffice";
+        public static final String COLUMN_POSTER = "poster";
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mmdp/MediaDB.java
@@ -0,0 +1,4 @@
+package com.example.mmdp;
+
+public class MediaDB {
+}
